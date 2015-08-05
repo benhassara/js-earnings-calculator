a8cd7860c84f5ec28dfa846092cc17930e9451fe
benhassara <bhassara@gmail.com>
2015-08-05 13:05:04 -0600

    calculator works, refactored into OOP

--- js/main.js
+++ js/main.js
@@ -2,13 +2,62 @@
 
 $(document).on('ready', function() {
   // console.log('sanity check!');
+  var totalEarnings = new Earnings();
 
+  $('form input').first().trigger('focus');
+
+  //submit form listener
   $('form[name="mealForm"]').on('submit', function(e){
     e.preventDefault();
-    var price = $('input[name="meal-price"').val();
-    var taxRate = $('input[name="meal-taxrate"]').val();
-    var tipRate = $('input[name="meal-tiprate"]').val();
 
-    console.log(price, taxRate, tipRate);
+    //grab values
+    var price = Number($('input[name="meal-price"').val());
+    var taxRate = Number($('input[name="meal-taxrate"]').val());
+    var tipRate = Number($('input[name="meal-tiprate"]').val());
+    //new Bill obj
+    var bill = new Bill(price, taxRate, tipRate);
+    //cache dom locations
+    var perCust = $('.totals:eq(0)').children();
+    var earnings = $('.totals:eq(1)').children();
+
+    //update tallys
+    totalEarnings.tips += bill.calcTip();
+    totalEarnings.meals++;
+
+    //add vals for bill to Total Charges
+    bill.append(perCust);
+
+    //update Total Earnings
+    totalEarnings.append(earnings);
+
+    //clear inputs and return focus to first input
+    $('form input[value="Clear"]').trigger('click');
+  });
+
+  //listener for Clear btn on form
+  $('form input[value="Clear"]').on('click', function(e){
+    e.preventDefault();
+    var inputs = $('form .form-input');
+
+    inputs.each(function(){
+      $(this).val('');
+    });
+
+    $('form input').first().trigger('focus');
+  });
+
+  //listener for Reset btn
+  //resets running totals
+  $('.text-center input').on('click', function(e){
+    e.preventDefault();
+    var tmpBill = new Bill();
+
+    reset($('.earn'), totalEarnings.labels);
+    reset($('.bill'), tmpBill.labels);
+
+    totalEarnings.tips = 0;
+    totalEarnings.meals = 0;
+
+    $('form input[value="Clear"]').trigger('click');
   });
 });
--- js/utility.js
+++ js/utility.js
@@ -1,17 +1,90 @@
 
 //Bill class
-function Bill(subtotal, taxRate, tipRate) {
-  this.subtotal = subtotal;
-  this.taxRate = taxRate;
-  this.tipRate = tipRate;
+function Bill(price, taxRate, tipRate) {
+  this.taxRate = taxRate * 0.01;
+  this.tipRate = tipRate * 0.01;
+  this.price = price;
+  this.labels = ['Subtotal: ', 'Tip: ', 'Total: '];
 }
 
+//Bill methods
 Bill.prototype.calcTax = function() {
   //calculate dollar amount for tax
-  return this.subtotal * this.taxRate;
+  return this.price * this.taxRate;
 };
 
 Bill.prototype.calcTip = function() {
   //calculate dollar amount for tip
-  return  this.subtotal * this.tipRate;
+  return this.price * this.tipRate;
 };
+
+Bill.prototype.calcTotal = function() {
+  //calculate total
+  return this.price + this.calcTax() + this.calcTip();
+};
+
+Bill.prototype.calcSub = function() {
+  //calculate subtotal
+  return this.price + this.calcTax();
+};
+
+Bill.prototype.toArray = function() {
+  //return array with: [subtotal, tip, total]
+  return [this.calcSub(), this.calcTip(), this.calcTotal()];
+};
+
+Bill.prototype.append = function(elems) {
+  //modify DOM to insert data for 1 Bill
+  var labels = this.labels;
+  var vals = this.toArray();
+
+  elems.each(function(i){
+    $(this).html('<p class="bill">' + labels[i] + ' $' + vals[i].toFixed(2) + '</p>');
+  });
+
+  return elems;
+};
+
+//Earnings class
+//hold running total for tips and num meals
+function Earnings() {
+  this.tips = 0;
+  this.meals = 0;
+  this.labels = ['Total Tips: ', 'Meal count: ', 'Average tip: '];
+}
+
+Earnings.prototype.avgTip = function() {
+  //calculate avg tip per bill
+  return this.tips / this.meals;
+};
+
+Earnings.prototype.toArray = function() {
+  //represent values in array
+  return [this.tips, this.meals, this.avgTip()];
+};
+
+Earnings.prototype.append = function(elems) {
+  //modify DOM to update running totals
+  var labels = this.labels;
+  var vals = this.toArray();
+
+  elems.each(function(i){
+    if (i === 1) {
+      $(this).html('<p class="earn">' + labels[i] + ' ' + vals[i] + '</p>');
+    }
+    else {
+      $(this).html('<p class="earn">' + labels[i] + ' $' + vals[i].toFixed(2) + '</p>');
+    }
+  });
+};
+
+//general utility funcs
+function reset(elems, labels) {
+  //reset the elems with the labels
+  //elems is jQuery selection
+  //labels is array of strings
+
+  elems.each(function(i){
+    $(this).html('<p>' + labels[i] + '</p>');
+  });
+}
